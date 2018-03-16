15ba6e4df49b691acd2187fd5a4566f6e956854d
Nadan Gergeo <nadan@kth.se>
2018-03-16 03:06:54 +0100

    Cinema now uses view component like all other demos

--- demo/src/views/Cinema/index.js
+++ demo/src/views/Cinema/index.js
@@ -3,9 +3,11 @@ import PropTypes from 'prop-types';
 import elementResizeDetector from "element-resize-detector";
 
 import Pyramid from "../../../../src";
-import Cover from "../../../../src/components/Cover";
+import View from "../../../../src/hocs/View";
 import Film from "../../components/Film";
-import films from "../../data/films";
+import filmsData from "../../data/films";
+
+const PyramidView = View(Pyramid);
 
 export default class Cinema extends React.Component {
 	static propTypes = {
@@ -16,45 +18,15 @@ export default class Cinema extends React.Component {
 		zoomable: true
 	};
 
-	constructor(props) {
-		super(props);
-
-		// Create a elementResizeDetector.
-		this.erd = props.erd || elementResizeDetector({strategy: "scroll"});
-
-		this.filmsData = films;
-	}
-
 	render() {
-
-		const pyramidStyle = {
-			height: "calc(100% - 100px)",
-			top: "100px",
-			zIndex: 1000,
-			// height: "100%",
-			// position: "static"
-		};
-
-		const films = this.filmsData.map( (data, index) => {
+		const films = filmsData.map( (data, index) => {
 			return (<Film data={data} key={index} themeColor={this.props.themeColor} />);
 		});
 
-		let filmsPyramid;
-		if(this.props.zoomedIn || this.props.zoomingIn || this.props.zoomingOut) {
-			filmsPyramid = (
-				<Pyramid erd={this.erd} style={pyramidStyle} extraPaddingTop="0">
-					{films}
-				</Pyramid>
-			);
-		} else {
-			filmsPyramid = null;
-		}
-
 		return (
-			<div className="demo">
-				<Cover {...this.props} zIndex={!this.props.zoomedOut ? 2000 : 100}>Cinema</Cover>
-				{filmsPyramid}
-			</div>
+			<PyramidView {...this.props}>
+				{films}
+			</PyramidView>
 		);
 	}
 }
