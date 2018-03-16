404cb8af9b5ae4a21d7ccb158e5a27dcf7ea312f
Nadan Gergeo <nadan@kth.se>
2018-03-16 04:06:15 +0100

    a lil cleanup + added forgotten title to Cinema demo

--- demo/src/index.js
+++ demo/src/index.js
@@ -14,23 +14,16 @@ import Gallery from "./views/Gallery";
 import AdvancedGallery from "./views/AdvancedGallery";
 
 class Demo extends React.Component {
-	constructor(props) {
-		super(props);
-
-		// Create a elementResizeDetector.
-		this.erd = props.erd || elementResizeDetector({strategy: "scroll"});
-	}
-
 	render() {
 		const pyramidStyle = {
 			height: "100%"
 		};
 
 		return (
-			<Pyramid padding="0" gutter="0" numberOfColumns="1" erd={this.erd} style={pyramidStyle}>
+			<Pyramid padding="0" gutter="0" numberOfColumns="1" style={pyramidStyle}>
 				<Images title="Images"/>
 				<Giphy title="Giphy" themeColor="#7e00ff"/>
-				<Cinema themeColor="#ff1b00" />
+				<Cinema title="Cinema" themeColor="#ff1b00" />
 				<Docs title="Docs" themeColor="#ffba00"/>
 				<Gallery title="Gallery" themeColor="#12b886"/>
 				<AdvancedGallery title="Advanced Gallery" themeColor="#4c6ef5"/>
